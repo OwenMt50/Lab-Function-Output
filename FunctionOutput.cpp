41dfc4ccf64017f8e6eefde2118d9a5b700e8903
OwenMt50 <Omartin005@student.butte.edu>
2025-03-26 05:46:26 +0000

    commit

--- FunctionOutput.cpp
+++ FunctionOutput.cpp
@@ -10,19 +10,19 @@ using namespace std;
 int main(){
     int Sold = 0;
     float answer = 0;
-
-    cout<<"*Commssion Table*\n";
+//The next 3 output lines give commission table, and asking input from user
+    cout<<"*Commission Table*\n"; 
     cout<<"<$50k = 4% | $50k - 250k = 6% | >$250k = 6.75%\n";
     cout<<"Enter the amount of money you have sold in the last work month.\n";
 
-    cin>>Sold;
+    cin>>Sold; //input of how much $ user made 
     if (Sold<50000){
-    answer = Sold*.04;
+    answer = Sold*.04; //less than 50k, then multiplied by 4%
     }else if(Sold>50000 & Sold<=250000){
-        answer = Sold*.06;
+        answer = Sold*.06; //more than 50k and less than or equal to 250k, multiplied by 6%
     }else if(Sold>250000){
-        answer = Sold*.0675;
+        answer = Sold*.0675; //more than 250k, multiplied by 6.75%
     }
 
-    cout<<"You have made $"<<answer<<" from commission.";
+    cout<<"You have made $"<<answer<<" from commission."; //message displayed to show how much money earnt from commission to user
 }
