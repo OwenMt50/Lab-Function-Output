ff214cb4537b4b2eac956f95676710032b12f076
OwenMt50 <Omartin005@student.butte.edu>
2025-03-28 07:19:22 +0000

    commit

--- FunctionOutput.cpp
+++ FunctionOutput.cpp
@@ -7,6 +7,20 @@
 
 using namespace std;
 
+float commission(int Sold, float answer){
+
+    int sold; //takes input from int main and into commission function
+
+    if (Sold<50000){
+        answer = Sold*.04; //less than 50k, then multiplied by 4%
+        }else if(Sold>50000 & Sold<=250000){
+            answer = Sold*.06; //more than 50k and less than or equal to 250k, multiplied by 6%
+        }else if(Sold>250000){
+            answer = Sold*.0675; //more than 250k, multiplied by 6.75%
+        }
+        return answer;
+}
+
 int main(){
     int Sold = 0;
     float answer = 0;
@@ -15,14 +29,7 @@ int main(){
     cout<<"<$50k = 4% | $50k - 250k = 6% | >$250k = 6.75%\n";
     cout<<"Enter the amount of money you have sold in the last work month.\n";
 
-    cin>>Sold; //input of how much $ user made 
-    if (Sold<50000){
-    answer = Sold*.04; //less than 50k, then multiplied by 4%
-    }else if(Sold>50000 & Sold<=250000){
-        answer = Sold*.06; //more than 50k and less than or equal to 250k, multiplied by 6%
-    }else if(Sold>250000){
-        answer = Sold*.0675; //more than 250k, multiplied by 6.75%
-    }
-
-    cout<<"You have made $"<<answer<<" from commission."; //message displayed to show how much money earnt from commission to user
+    cin>>Sold;
+    float commission(int Sold, float answer); //if function call
+    cout<<"You have made $"<<commission(Sold, answer); //output answer to user
 }
